7581ae2a045af6c0bb4bda5093a210f8077cb8c8
pot <57581179+pot0rpan@users.noreply.github.com>
2020-09-25 14:25:40 -0600

    Alert owner on error

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-1. Set `BOT_TOKEN` and `SCREWS_API_KEY` in .env file at project root
+1. Set `BOT_TOKEN` and `OWNER_DISCORD_ID` in .env file at project root
+
+2. Add `SCREWS_API_KEY` to .env if available
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 
-const { PREFIX } = require('./config');
+const { PREFIX, COLOR_DANGER } = require('./config');
 const botCommands = require('./commands');
 
 const bot = new Discord.Client();
@@ -22,14 +22,34 @@ bot.on('message', (msg) => {
   const prefix = (args.shift() || '').toLowerCase();
   const command = (args.shift() || '').toLowerCase();
 
-  if (msg.author.bot || prefix !== PREFIX || !bot.commands.has(command)) {
+  if (msg.author.bot || prefix !== PREFIX) {
     return;
   }
 
+  if (!bot.commands.has(command)) {
+    return bot.commands.get('help').execute(msg);
+  }
   try {
     bot.commands.get(command).execute(msg, args);
   } catch (err) {
     console.error(err);
-    msg.reply('Uh oh, there was an error executing that command.');
+
+    const owner = bot.users.cache.get(process.env.OWNER_DISCORD_ID);
+
+    msg.channel.send(
+      'Uh oh, there was an error executing that command.' +
+        (owner ? " I'll alert my owner." : '')
+    );
+
+    if (!owner) return;
+
+    const embed = new Discord.MessageEmbed({
+      title: 'Hey something broke',
+      description: 'Command: `' + `${command} ${args.join(' ')}` + '`',
+      color: COLOR_DANGER,
+      timestamp: Date.now(),
+    });
+
+    owner.send(embed).catch((err) => console.error(err));
   }
 });
